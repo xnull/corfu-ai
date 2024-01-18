500975f7291e50529deffc1632288b409647de9c
Viacheslav <xrw.null@gmail.com>
2024-01-17 23:13:53 -0800

    fix

--- ai-lap/ai/ai_app.py
+++ ai-lap/ai/ai_app.py
@@ -64,7 +64,7 @@ def run_llm(input_data, question):
     model_inputs = tokenizer(prompt, return_tensors="pt")
     generated_ids = llama_model.generate(**model_inputs, max_new_tokens=1000, do_sample=True)
 
-    res = tokenizer.batch_decode(generated_ids)[0]
+    res = tokenizer.batch_decode(generated_ids)
 
     return res[0]
 
